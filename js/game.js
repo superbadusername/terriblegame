8016fcf725ab3a358a6f7e8007584b907987761d
superbadusername <35783853+superbadusername@users.noreply.github.com>
2018-01-31 14:14:20 +0000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -86,6 +86,18 @@ function updateCookieText(){
 }
 
 function clicked(){
+	$("#cookieImage").stop(true,true)
+	$("#cookieImage").css({
+		
+	})
+	$("#cookieImage").animate({
+		height: '+=20px',
+		width: '+=20px'
+	}, 65)
+	$("#cookieImage").animate({
+		height: '-=20px',
+		width: '-=20px'
+	}, 65)
 	cookies += amountPerClick;
 	updateCookieText();
 }
